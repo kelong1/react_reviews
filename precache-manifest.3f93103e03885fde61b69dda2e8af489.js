db5e05e5a11bb674d7e554c1eb56391e044e9b8d
kelong1 <49603313+kelong1@users.noreply.github.com>
2021-10-25 14:38:41 +0300

    Updates

---
+++ precache-manifest.3f93103e03885fde61b69dda2e8af489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0e52e9014546c3424b5b6d4f39ecef",
+    "url": "/react_reviews/index.html"
+  },
+  {
+    "revision": "159a0309e3bcd70e984f",
+    "url": "/react_reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/react_reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159a0309e3bcd70e984f",
+    "url": "/react_reviews/static/js/main.620688a3.chunk.js"
+  },
+  {
+    "revision": "3832393d75c7fef48168",
+    "url": "/react_reviews/static/js/runtime-main.ad9aceac.js"
+  }
+]);
